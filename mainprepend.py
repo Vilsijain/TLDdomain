ad92d8abea5e74e5a28c089195e7413e860287af
Vilsijain <93503754+Vilsijain@users.noreply.github.com>
2022-06-15 23:18:03 +0530

    Update mainprepend.py

--- mainprepend.py
+++ mainprepend.py
@@ -6,12 +6,12 @@ from flask import Flask,jsonify
 app = Flask(__name__)
 
 @app.route('/<string:words>')
-def prepend(words):
+def append(words):
     result=[]
-    str2 = ['ve', 'bright', 'toffee', 'code', 'community', 'dev', 'eat', 'drink', 'repeat']
-    for i in random.sample(str2,5):
-        strfin=i+words
-        result.prepend(strfin) 
+    str2 = ['.com',  '.net', '.org', '.Whois Privacy']
+    for i in random.sample(str2,4):
+        strres=words+i
+        result.append(strres)
     return jsonify(result)
 if __name__ == '__main__':
     port = os.getenv('PORT',None) or 80
